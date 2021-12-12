b8aca719b4902d5d553f8c6b7b2822b6308a8942
NoviScl <sichenglei1125@gmail.com>
2021-12-11 22:30:13 -0500

    GPR

--- parse_preds.py
+++ parse_preds.py
@@ -1,10 +1,19 @@
 import json
 
-with open("predictions/QANTA_test_PromptRetrieve_preds.json", "r") as f:
-    data = json.load(f)
+with open("predictions/QANTA_test_GPT_preds.json", "r") as f:
+    data1 = json.load(f)
 
-for dp in data:
-    print ("pred: ", dp["prediction"])
-    print ("answer: ", dp["gold_answer"])
-    print ()
+with open("predictions/QANTA_test_PromptRetrieve_preds.json", "r") as f:
+    data2 = json.load(f)
+
+
+for i in range(len(data1)):
+    dp = data1[i]
+    dp2 = data2[i]
+    if dp["prediction"] != dp2["prediction"]:
+        print ("question: ", dp["question"])
+        print ("pred  GPT: ", dp["prediction"])
+        print ("pred GPR: ", dp2["prediction"])
+        print ("answer: ", dp["gold_answer"])
+        print ()
 
